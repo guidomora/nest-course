e9f9fffcd10451100602b8ce228b885f083c33a0
guidomora <guidomorabito161@hotmail.com>
2025-01-19 20:17:33 -0300

    first commit

---
+++ src/cars/cars.module.ts
@@ -0,0 +1,10 @@
+import { Get, Module } from '@nestjs/common';
+import { CarsController } from './cars.controller';
+
+@Module({
+  controllers: [CarsController]
+})
+export class CarsModule {
+
+
+}
